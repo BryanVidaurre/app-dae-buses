d8dad87e4336d78a5b01c24e3f00770530ec5fb1
Bryan J. Vidaurre <119612480+BryanVidaurre@users.noreply.github.com>
2026-01-09 11:54:34 -0300

    prueba commit

--- app/src/main/java/com/example/pda/MainActivity.kt
+++ app/src/main/java/com/example/pda/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.pda.ui.theme.PDATheme
 
+//verificar commit
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
